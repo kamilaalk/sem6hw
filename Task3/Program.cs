32d7651d99bda4391a6459385b0afc9f06a678b7
Kamila <kamila.sbz@yandex.com>
2024-03-21 16:17:24 +0500

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+string input = "1234";
+bool isPalindrome = IsPalindrome(input);
+Console.WriteLine(isPalindrome ? "Да" : "Нет");
+
+bool IsPalindrome(string str)
+{
+string normal = new
+string(str.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+return normal.SequenceEqual(normal.Reverse());
+}
